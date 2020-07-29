2c53c181c400f1c7b8bf076250a17fd7b6ff29f1
tunchasan <hasantunc_ogr@outlook.com>
2020-07-30 00:04:16 +0300

    Implemented main page' components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import './App.css'
 import Header from './Header';
+import Sidebar from './Sidebar';
+import RecommendedVideos from './RecommendedVideos';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Youtube</h1>
-
       <Header/>
+      <div className="app_page">
+        <Sidebar/>
+        <RecommendedVideos/>
+      </div>
       
-      {/* Header */}
-      {/* Slider */}
-      {/* RecommendedVideos */}
-
     </div>
   );
 }
