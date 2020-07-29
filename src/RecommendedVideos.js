26001b590df4b350923c3cdc687dc1230de8514d
tunchasan <hasantunc_ogr@outlook.com>
2020-07-30 00:05:43 +0300

    Created RecommendedVideos component

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RecommendedVideos.css';
+
+function RecommendedVideos() {
+    return (
+        <div className='recommendedVideos'>
+            <h1>This is where videos go</h1>
+        </div>
+    )
+}
+
+export default RecommendedVideos;
