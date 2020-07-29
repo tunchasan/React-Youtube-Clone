ea278fd6eeeb9711997e8a9436a91ef41a05bf63
tunchasan <hasantunc_ogr@outlook.com>
2020-07-29 21:26:35 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Youtube</h1>
+
+      <Header/>
+      
+      {/* Header */}
+      {/* Slider */}
+      {/* RecommendedVideos */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQDKHjBggWtpweJWYEOrkkc-_l-KQhW6o",
+  authDomain: "clone-48796.firebaseapp.com",
+  databaseURL: "https://clone-48796.firebaseio.com",
+  projectId: "clone-48796",
+  storageBucket: "clone-48796.appspot.com",
+  messagingSenderId: "200493330925",
+  appId: "1:200493330925:web:11d9ee93219ee1b423f7e4",
+  measurementId: "G-EXQCBB5KYD"
+};
