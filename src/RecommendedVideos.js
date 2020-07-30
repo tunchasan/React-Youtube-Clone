0578f5439bc2252b3f24e68a4f53ffa1bfbf81f1
tunchasan <hasantunc_ogr@outlook.com>
2020-07-30 20:04:26 +0300

    Added VideoCard to Recommended area

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,77 @@
 import React from 'react'
+import VideoCard from './VideoCard';
 import './RecommendedVideos.css';
 
 function RecommendedVideos() {
     return (
         <div className='recommendedVideos'>
-            <h1>This is where videos go</h1>
+            <h2>Recommended</h2>
+            <div className="recommendedVideos_videos">
+                <VideoCard
+                    title="React Courses"
+                    views="2.3M Views"
+                    timestapms="3 days ago"
+                    channelImage="https://avatars1.githubusercontent.com/u/39636292?s=460&u=a29bef077d71d9e8eff9624c63d6451cbd8f79df&v=4"
+                    channel="Hasan Tunc"
+                    image="https://i.ytimg.com/vi/DLX62G4lc44/maxresdefault.jpg"
+                />
+                <VideoCard
+                    title="React Courses"
+                    views="2.3M Views"
+                    timestapms="3 days ago"
+                    channelImage="https://avatars1.githubusercontent.com/u/39636292?s=460&u=a29bef077d71d9e8eff9624c63d6451cbd8f79df&v=4"
+                    channel="Hasan Tunc"
+                    image="https://i.ytimg.com/vi/DLX62G4lc44/maxresdefault.jpg"
+                />
+                <VideoCard
+                    title="React Courses"
+                    views="2.3M Views"
+                    timestapms="3 days ago"
+                    channelImage="https://avatars1.githubusercontent.com/u/39636292?s=460&u=a29bef077d71d9e8eff9624c63d6451cbd8f79df&v=4"
+                    channel="Hasan Tunc"
+                    image="https://i.ytimg.com/vi/DLX62G4lc44/maxresdefault.jpg"
+                />
+                <VideoCard
+                    title="React Courses"
+                    views="2.3M Views"
+                    timestapms="3 days ago"
+                    channelImage="https://avatars1.githubusercontent.com/u/39636292?s=460&u=a29bef077d71d9e8eff9624c63d6451cbd8f79df&v=4"
+                    channel="Hasan Tunc"
+                    image="https://i.ytimg.com/vi/DLX62G4lc44/maxresdefault.jpg"
+                />
+                <VideoCard
+                    title="React Courses"
+                    views="2.3M Views"
+                    timestapms="3 days ago"
+                    channelImage="https://avatars1.githubusercontent.com/u/39636292?s=460&u=a29bef077d71d9e8eff9624c63d6451cbd8f79df&v=4"
+                    channel="Hasan Tunc"
+                    image="https://i.ytimg.com/vi/DLX62G4lc44/maxresdefault.jpg"
+                />
+                <VideoCard
+                    title="React Courses"
+                    views="2.3M Views"
+                    timestapms="3 days ago"
+                    channelImage="https://avatars1.githubusercontent.com/u/39636292?s=460&u=a29bef077d71d9e8eff9624c63d6451cbd8f79df&v=4"
+                    channel="Hasan Tunc"
+                    image="https://i.ytimg.com/vi/DLX62G4lc44/maxresdefault.jpg"
+                />
+                <VideoCard
+                    title="React Courses"
+                    views="2.3M Views"
+                    timestapms="3 days ago"
+                    channelImage="https://avatars1.githubusercontent.com/u/39636292?s=460&u=a29bef077d71d9e8eff9624c63d6451cbd8f79df&v=4"
+                    channel="Hasan Tunc"
+                    image="https://i.ytimg.com/vi/DLX62G4lc44/maxresdefault.jpg"
+                />
+                <VideoCard
+                    title="React Courses"
+                    views="2.3M Views"
+                    timestapms="3 days ago"
+                    channelImage="https://avatars1.githubusercontent.com/u/39636292?s=460&u=a29bef077d71d9e8eff9624c63d6451cbd8f79df&v=4"
+                    channel="Hasan Tunc"
+                    image="https://i.ytimg.com/vi/DLX62G4lc44/maxresdefault.jpg"
+                />
+            </div>           
         </div>
     )
 }
